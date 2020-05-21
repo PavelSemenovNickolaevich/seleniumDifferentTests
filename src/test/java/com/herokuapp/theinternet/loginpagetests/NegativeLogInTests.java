a8ae0e23b014ed8169c9cac031464cfb2f7c5540
test <natitshow@gmail.com>
2020-05-21 12:11:20 +0300

    added new change (new way) with data driven

--- src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -1,5 +1,6 @@
 package com.herokuapp.theinternet.loginpagetests;
 
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -8,28 +9,39 @@ import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
 
+import java.util.Map;
+
 public class NegativeLogInTests extends TestUtilities {
 
-	@Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1)
-	public void negativeLogInTest(String username, String password, String expectedErrorMessage) {
-		log.info("Starting negativeTest");
+    //	@Parameters({ "username", "password", "expectedMessage" })
+    @Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+    //public void negativeLogInTest(String username, String password, String expectedErrorMessage){
+    public void negativeLogInTest(Map<String, String> testData) {
+        //Data
+        String no = testData.get("no");
+        String username = testData.get("username");
+        String password = testData.get("password");
+        String expectedErrorMessage = testData.get("expectedMessage");
+        String description = testData.get("description");
 
-		// open main page
-		WelcomePage welcomePage = new WelcomePage(driver, log);
-		welcomePage.openPage();
 
-		// Click on Form Authentication link
-		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+        log.info("Starting negativeLogInTest #" + no + " for " + description );
 
-		// execute negative login
-		loginPage.negativeLogIn(username, password);
+        // open main page
+        WelcomePage welcomePage = new WelcomePage(driver, log);
+        welcomePage.openPage();
 
-		// wait for error message
-		loginPage.waitForErrorMessage();
-		String message = loginPage.getErrorMessageText();
+        // Click on Form Authentication link
+        LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
 
-		// Verification
-		Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
-	}
+        // execute negative login
+        loginPage.negativeLogIn(username, password);
+
+        // wait for error message
+        loginPage.waitForErrorMessage();
+        String message = loginPage.getErrorMessageText();
+
+        // Verification
+        Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
+    }
 }
