104acac29ae93cd07d3506bbcae87edc197f4909
test <natitshow@gmail.com>
2020-05-22 12:07:09 +0300

    added examples with screenshots

--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -1,5 +1,14 @@
 package com.herokuapp.theinternet.base;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestUtilities extends BaseTest {
 
@@ -19,5 +28,34 @@ public class TestUtilities extends BaseTest {
                 {1, "logo.png"},
                 {3, "text.txt"}
         };
+    }
+
+    /** Take screenshot */
+    protected void takeScreenshot(String fileName) {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir")
+                + File.separator + "test-output"
+                + File.separator + "screenshots"
+                + File.separator + getTodaysDate()
+                + File.separator + testSuiteName  // addtest suite name
+                + File.separator + testName       // adding testName
+                + File.separator + testMethodName //adding test method
+                + File.separator + getSystemTime()
+                + " " + fileName + ".png";
+        try {
+            FileUtils.copyFile(scrFile, new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Todays date in yyyyMMdd format */
+    private static String getTodaysDate() {
+        return (new SimpleDateFormat("yyyyMMdd").format(new Date()));
+    }
+
+    /** Current time in HHmmssSSS */
+    private String getSystemTime() {
+        return (new SimpleDateFormat("HHmmssSSS").format(new Date()));
     }
 }
